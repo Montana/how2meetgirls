da7363e48f8e891c855862f95f6a5164299a42db
Michael Mendy <montana@linux.com>
2024-02-20 05:53:25 -0800

    obvs a joke

---
+++ 1_liners.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# You could in theory put like sleep 10 in between each one liner so it doesn't just appear like you ran `cat one_liners.sh`.
+
+echo "Are you a magician? Because whenever I look at you, everyone else disappears."
+echo "Do you have a name, or can I call you mine?"
+echo "Excuse me, but I think you dropped something: my jaw."
+echo "Are you made of copper and tellurium? Because you're Cu-Te."
+echo "Do you believe in love at first sight, or should I walk by again?"
+echo "Are you Wi-Fi? Because I'm feeling a connection."
+echo "Is there an airport nearby or is it my heart taking off?"
+echo "If you were a vegetable, you'd be a cute-cumber!"
+echo "Do you have a map? I just got lost in your eyes."
+echo "Is your name Google? Because you have everything I've been searching for."
